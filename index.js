e28493083fad7de8a272f4f725afd4e5f21b0fea
Nevylish <nevylish@pm.me>
2022-04-02 08:07:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
-var fs = require('fs');
-var uniqueRandomArray = require('unique-random-array');
-var wordListPath = __dirname + '/words.txt';
+const fs = require('fs');
+const uniqueRandomArray = require('unique-random-array');
+const wordListPath = __dirname + '/words.txt';
 
 module.exports = uniqueRandomArray(fs.readFileSync(wordListPath, 'utf8').split('\n'));
